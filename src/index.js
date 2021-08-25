38c3ff928a3d3ea66334f25ed9ef0c17229ed862
Ashley Buono <ashleygbuono@gmail.com>
2021-08-25 17:07:16 -0400

    holy delayed commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './app';
+// import React from 'react';
+// import { render } from 'react-dom';
+// import App from './app';
 
-const app = document.querySelector('#app');
+// const app = document.querySelector('#app');
 
-render(<App />, app);
+// render(<App />, app);
